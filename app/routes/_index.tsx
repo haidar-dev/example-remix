e2c5175c7a0f9cb25da078b3a3ec917f5b00c4b7
M Haidar Hanif <git@mhaidarhanif.com>
2023-09-23 17:28:09 +0700

    feat: refine website content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,33 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Example: Remix" },
+    { name: "description", content: "Remix full stack web app example" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
+    <div>
+      <header>
+        <h1>Example: Remix</h1>
+      </header>
+
+      <main>
+        <p>
+          Web: <a href="https://remix.haidar.dev">remix.haidar.dev</a>
+        </p>
+        <p>
+          Repo:{" "}
+          <a href="https://github.com/haidar-dev/example-remix">
+            github.com/haidar-dev/example-remix
           </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+        </p>
+      </main>
+
+      <footer>
+        <p>&copy; haidar.dev</p>
+      </footer>
     </div>
   );
 }
